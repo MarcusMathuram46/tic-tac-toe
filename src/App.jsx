9e9ceac68a00c86187e1765c594fbf446fa40565
Marcus Mathuram <marcus.mathuram7@gmail.com>
2024-03-11 23:15:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/App.css'
+function App() {
+  return (
+    <div>
+      <button>
+        <h1>Hello World!</h1>
+      </button>
+    </div>
+  )
+}
+
+export default App
